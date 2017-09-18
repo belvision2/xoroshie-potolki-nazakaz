0f459cc080ce2ce500f1a6a6baf83a7aede6694d
sergeigaev <belvision2@users.noreply.github.com>
2017-09-18 18:55:32 +0300

    Правки по сайту до обработчика форм

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -137,3 +137,15 @@
 			});
 	}
 })(jQuery);
+$(function($) {
+
+
+    var time = new Date();
+    var target_time = new Date(time.getFullYear(), time.getMonth(), time.getDate());
+    target_time = target_time.valueOf()+1000*60*60*24;
+    var tomorrow = new Date(target_time);
+    var months = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'];
+    $('.end-of-action-date').html(tomorrow.getDate()+" "+months[tomorrow.getMonth()]);
+    // $('#timer2 .text').html("Предложение действительно до "+tomorrow.getDate()+" "+months[time.getMonth()]);
+
+});
